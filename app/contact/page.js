094459126aca4217c5bed6b5745853b47b1c2bc1
yasasipathirana <160473334+yasasipathirana@users.noreply.github.com>
2024-10-28 19:37:29 +0530

    Error fixing

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,9 +1,10 @@
 import styles from '../../styles/Contact.module.css';
 
+
 export default function ContactPage() {
   return (
     <div>
-      <p>If you'd like to get in touch, please reach out via the following:</p>
+      <p>If you&apos;d like to get in touch, please reach out via the following:</p>
       <ul>
         <li>Email: <a href="mailto:yasasipathirana123@gmail.com" className={styles.contactLink}>yasasipathirana123@gmail.com</a></li>
         <li>LinkedIn: <a href="https://www.linkedin.com/in/yasasi-pathirana-a89412300/" target="_blank" rel="noopener noreferrer" className={styles.contactLink}>My LinkedIn</a></li>
